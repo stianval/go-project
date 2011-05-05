c3490803b0cdf59f517479eefee1ddd77dd2506b
TZer0 <TZer0@localhost>
2011-05-05 22:56:39 +0200

    Rules.

--- src/game.cpp
+++ src/game.cpp
@@ -25,6 +25,10 @@ double dx, dy;
 
 Mesh mesh;
 
+bool place_stone(int x, int y, int side);
+bool check_freedoms(int x, int y, int side);
+bool handle_flags(bool removePiece);
+
 void draw_board() {
 	int i;
 	glBegin(GL_POLYGON);
@@ -58,22 +62,22 @@ void game_init (int argc, char *argv[])
 	xfields = yfields = 19;
 	dx = 1.8/(xfields-1);
 	dy = 1.8/(yfields-1);
-	
+
 	board = new int*[yfields];
 	for(int y=0;y<yfields; y++) {
 		board[y] = new int[xfields];
 		for(int x=0; x<xfields; x++)
 			board[y][x] = 0;
 	}
-	
+
 	// should be substituted by a os_init or win_init function.
 #ifdef _WIN32
 	WSADATA wsaData;
 	if (WSAStartup(MAKEWORD(2,0), &wsaData) != 0)
-	  {
+	{
 		fprintf (stderr, "WSAStartup failed.\n");
 		exit (EXIT_FAILURE);
-	  }
+	}
 
 	glGenBuffers = (PFNGLGENBUFFERSPROC)
 		wglGetProcAddress ("glGenBuffers");
@@ -82,7 +86,7 @@ void game_init (int argc, char *argv[])
 	glBufferData = (PFNGLBUFFERDATAPROC)
 		wglGetProcAddress ("glBufferData");
 #endif
-	
+
 	if (argc < 2){
 		player = 0;
 		rq_sock = init_server();
@@ -100,7 +104,7 @@ void game_init (int argc, char *argv[])
 	}
 
 	mesh.load("triangle.obj");
-	
+
 }
 
 void set_board_position(int x,int y)
@@ -116,21 +120,21 @@ void game_display() {
 	glLoadIdentity();
 	gluOrtho2D(-1,1,1,-1);
 	//gluPerspective(60.0, 1, 0.01, 100);
-	
+
 	//glMatrixMode(GL_MODELVIEW);
 	//glLoadIdentity();
 	//gluLookAt(1,1,1, 0,0,0, 0,0,1);
-	
+
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	draw_board();
-	
+
 	for(int x=0; x<xfields; x++)
 		for(int y=0; y<yfields; y++) {
 			if(board[y][x]) {
 				static float cols[] =
-					{0,0,0, 1,1,1};
-				
+				{0,0,0, 1,1,1};
+
 				set_board_position(x,y);
 				int bval = board[y][x]-1;
 				mesh.render(cols[bval*3], cols[bval*3+1], cols[bval*3+2]);
@@ -142,6 +146,7 @@ void game_display() {
 void game_keyboard(unsigned char key, int x, int y) {
 	switch(key) {
 		case 27:
+			close(sock);
 			exit(EXIT_SUCCESS);
 		default:
 			break;
@@ -155,6 +160,7 @@ void game_idle() {
 		case CmdPut:
 			turn = !turn;
 			board[action.y][action.x] = (!player)+1;
+			place_stone(action.x, action.y, (!player)+1);
 			glutPostRedisplay();
 			break;
 		case CmdRemove:
@@ -167,34 +173,37 @@ void game_idle() {
 }
 
 void game_mouse(int b, int z, int x, int y) {
-	if(!z) {
-		return;
-	}
-	if (turn != player) {
+	if(!z || turn != player) {
 		return;
 	}
 
 	double xd = (double(x)/SWIDTH - 0.5)*2.0 + 0.9;
 	double yd = (double(y)/SHEIGHT - 0.5)*2.0 + 0.9;
-	
+
 	int iX = xd/1.8*18.0 +0.5;
 	int iY = yd/1.8*18.0 +0.5;
-	
+
 	sPlayerAction action;
-	
+
 	//printf("%g %g\n", xd,yd);
 	if(iX < xfields && iY < yfields && iX>=0 && iY>=0) {
 		switch(b)
 		{
 			case GLUT_LEFT_BUTTON:
+				if (board[iY][iX] != 0) {
+					return;
+				}
 				board[iY][iX] = player+1;
+				if (place_stone(iX, iY, player+1)) {
+					return;
+				}
 				action.command = CmdPut;
 				action.x = iX;
 				action.y = iY;
 				turn = !turn;
 				send_command(sock, action);
 				break;
-				
+
 			case GLUT_RIGHT_BUTTON:
 				board[iY][iX] = 0;
 				action.command = CmdRemove;
@@ -204,5 +213,57 @@ void game_mouse(int b, int z, int x, int y) {
 				break;
 		}
 		glutPostRedisplay();
+	}
+}
+
+bool place_stone(int x, int y, int side){
+	int nx, ny;
+	bool ret = false;
+	for (int i = 0; i < 4; i++) {
+		nx = x+(i==1)-(i==0);
+		ny = y+(i==3)-(i==2);
+		ret |= handle_flags(!check_freedoms(nx, ny, 3-side));
+	}
+	//if (ret) {
+	//	return false;
+	//}
+	return handle_flags(!check_freedoms(x, y, side));
+ 
+}
+
+bool handle_flags(bool removePiece){
+	for (int i = 0; i < xfields; i++) {
+		for (int j = 0; j < yfields; j++) {
+			if (removePiece && (board[i][j] & 4)) {
+				board[i][j] = 0;
+			} else {
+				board[i][j] %= 4;
+			}
+		}
+	}
+	return removePiece;
+}
+
+bool check_freedoms(int x, int y, int side) {
+	if (x < 0 || x >= xfields || y < 0 || y >= yfields) { 
+		return false;
+	}
+	if (board[y][x] == 0) {
+		return true;
+	} else if (board[y][x]&4) { // check visited flag
+		return false;
+	} else if (board[y][x] == side) {
+		board[y][x] |= 4; // set visited flag
+		int nx, ny;
+		for (int i = 0; i < 4; i++) {
+			nx = x+(i==1)-(i==0);
+			ny = y+(i==3)-(i==2);
+			if (check_freedoms(nx, ny, side)) {
+				return true;
+			}
+		}
+		return false;
+	} else {
+		return false;
 	}
 }
